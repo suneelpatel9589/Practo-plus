10ad1fbd7d0ea8ac12bb92729a3c4bb24cafa69c
suneel patel <suneelpatel9589@gmail.com>
2026-05-13 14:04:47 +0530

    s

--- myenvfolder/practo_service/practo_service/settings.py
+++ myenvfolder/practo_service/practo_service/settings.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
+import pymysql
+pymysql.install_as_MySQLdb()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
