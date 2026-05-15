ed452a2b593220f625cdcca80f117926948d7e0d
suneel patel <suneelpatel9589@gmail.com>
2026-05-15 14:11:17 +0530

    Try Gmail SMTP SSL port

--- myenvfolder/practo_service/practo_service/settings.py
+++ myenvfolder/practo_service/practo_service/settings.py
@@ -146,9 +146,12 @@ SIMPLE_JWT = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = 10
+EMAIL_PORT = 465
+
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+
+EMAIL_TIMEOUT = 30
 
 EMAIL_HOST_USER = os.getenv("EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
