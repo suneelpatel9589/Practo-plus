5b6cfb49898cfd0c9b7f928fd360ff5970dc993e
suneelpatel9589 <suneelpatel9589@gmail.com>
2026-05-17 13:48:25 +0530

    Add doctor image url

--- myenvfolder/practo_service/practo_service/settings.py
+++ myenvfolder/practo_service/practo_service/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Media
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
