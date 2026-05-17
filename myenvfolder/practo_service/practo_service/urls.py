e23e0067eb7f260c8d5d76a3d7be7ac2ac9a9464
suneelpatel9589 <suneelpatel9589@gmail.com>
2026-05-17 10:33:28 +0530

    g

--- myenvfolder/practo_service/practo_service/urls.py
+++ myenvfolder/practo_service/practo_service/urls.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include
-=======
-from django.urls import path, include, re_path
->>>>>>> 6e15ac83889b8f98efc76dd0555c53024a1b7366
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -25,7 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
 
-<<<<<<< HEAD
     path(
         'swagger/',
         schema_view.with_ui('swagger', cache_timeout=0),
@@ -37,8 +32,4 @@ urlpatterns = [
         schema_view.with_ui('redoc', cache_timeout=0),
         name='redoc'
     ),
-=======
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
->>>>>>> 6e15ac83889b8f98efc76dd0555c53024a1b7366
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
